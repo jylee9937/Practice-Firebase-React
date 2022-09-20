e5c01f540a9a2e859f188b645cad82a8fd79bc81
jylee9937 <lkj49864@ajou.ac.kr>
2022-09-21 00:41:14 +0900

    :necktie: add login business logic

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+A
+package-lock.json,f/a/fa288d1472d29beccb489a676f68739ad365fc47
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+K
+src/components/AppRouter.js,d/2/d2c9e96fb5967c0e60c7dae582a725b7238d2884
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, {useState} from "react";
+import {authService} from "../firebaseInstance";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
 
 const Auth = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [newAccount, setNewAccount] = useState(true);
 	const onChange = (event) => {
 		const {target: {name, value}} = event;
 		if(name === "email"){
@@ -12,15 +15,27 @@ const Auth = () => {
 		}
 	}
 
-	const onSubmit = (event)=> {
+	const onSubmit = async (event)=> {
 		event.preventDefault();
+		try{
+			let data;
+			if(newAccount){
+				data = await createUserWithEmailAndPassword(authService, email, password);
+			}else{
+				data = await signInWithEmailAndPassword(authService, email, password);
+			}
+			console.log(data);
+
+		}catch(err){
+			console.log(err);
+		}
 	}
 	return(
 		<>
 			<form onSubmit={onSubmit}>
 				<input name="email" type={"email"} placeholder={"Email"} value={email} onChange={onChange} required/>
 				<input name="password" type={"password"} placeholder={"Password"} value={password}  onChange={onChange} required/>
-				<input type={"submit"} value={"Log In"}/>
+				<input type={"submit"} value={newAccount ? "Create Account" : "Log In"}/>
 			</form>
 			<div>
 				<button>
